71ccb3b94cc6edf43ae84e1045f0d9515323bbfd
Ulysse Durand <ulysse.durand@ens-lyon.fr>
2025-06-16 13:34:39 +0530

    good cv

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ class Config:
     def __init__(self):
         self.input_file = "infos.yml"
         self.yml_template_file = "cv_template.yml.j2"
-        self.html_template_file = "html_template.html.j2"
+        self.html_template_file = "cv_template.html.j2"
         self.yml_output_file = "cv.yml"
         self.html_output_file = "cv.html"
 
--- src/renderer.py
+++ src/renderer.py
@@ -1,6 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
 from jinja2.filters import do_dictsort
 import yaml
+import markdown
+import re
 import sys
 
 from src.config import Config, get_config
@@ -29,6 +31,9 @@ def build_html(config: Config, infos):
         trim_blocks=True,
         lstrip_blocks=True
     )
+    env.filters["mdlink"] = lambda text: re.sub(
+        r'\[([^\]]+)\]\(([^)]+)\)', r'<a href="\2">\1</a>', text
+    )
     template = env.get_template(config.html_template_file)
     rendered = template.render(**infos)
     return rendered
@@ -37,8 +42,8 @@ def main(config: Config):
     infos = load_infos(config)
     with open(config.yml_output_file, 'w') as f:
        f.write(build_yml(config, infos)) 
-    # with open(config.html_output_file, 'w') as f:
-    #    f.write(build_html(config, infos)) 
+    with open(config.html_output_file, 'w') as f:
+       f.write(build_html(config, infos)) 
 
 if __name__ == '__main__':
     config = get_config()
